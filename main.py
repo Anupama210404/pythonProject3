0ac426f93b2583e0ee85ae7e72a02e37256fb690
K.V.D.ANUPAMA <2100031087@kluniversity.in>
2022-08-03 16:14:33 +0530

    this is my commit line

---
+++ main.py
@@ -0,0 +1,8 @@
+list=['anu' , 786 , 2.23 , 'kamala' ,70.2]
+tinylist=[123,'kamala']
+print(list)           #prints complete list
+print(list[0])        #prints first element of the list
+print(list[1:3])      #prints elements starting from 2nd till 3rd
+print(list[2:])       #prints elements staring from 3rd element
+print(tinylist*2)     #prints list two times
+print(list+tinylist)  #prints concatenated lists
